1cebd08345e4ab293a465e30358a9c9c423e48f8
Arman Ali <toplinegeeks@gmail.com>
2022-09-22 11:42:12 +0500

    new structure for complex project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './layouts/Navbar'
+
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
